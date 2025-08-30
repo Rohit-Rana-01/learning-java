f631dda020d99eef8abbc17495866ec1ca096104
Rohit-Rana-01 <rohitrana3122000@gmail.com>
2025-08-30 19:56:47 +0530

    successfully created a code for favorit number

--- practice code/Main.java
+++ practice code/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        me.name();
-        me.age();
+    
+        Scanner src = new Scanner(System.in);
+        System.out.print("enter your name :");
+        String name = src.nextLine();
+        System.out.println("my name is " + name);
     }
 
 }
---
+++ practice code/favorit_no.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class favorit_no{
+public static void main(String[] args ){
+    Scanner src = new Scanner(System.in);
+    System.out.print("Eneter your favorit number :");
+    int num = src.nextInt();
+    System.out.println(num + " is my also favorit number .");
+
+}
+
+
+}
